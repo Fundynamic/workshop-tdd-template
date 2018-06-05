b43ac57f1e0fc7dd0a1db69ac621300950b21716
Stefan Hendriks <stefan@fundynamic.com>
2018-06-05 13:45:56 +0200

    Refactor : calculate score in score method and rolls in roll

--- src/main/java/com/fundynamic/workshop/Game.java
+++ src/main/java/com/fundynamic/workshop/Game.java
@@ -2,13 +2,19 @@ package com.fundynamic.workshop;
 
 public class Game {
 
-	private int score = 0;
+	private int currentRoll = 0;
+	private int rolls[] = new int[21]; // 9 frames 2 rolls, 10th frame possibly 3 rolls
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll] = pins;
+		currentRoll++;
 	}
 
 	public int score() {
+		int score = 0;
+		for (int roll = 0; roll < 21; roll++) {
+			score += rolls[roll];
+		}
 		return score;
 	}
 }
